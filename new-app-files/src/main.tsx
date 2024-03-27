42e4f423ad3d215936b54185dd4ec7abc5c84b35
Yanay Zabary <yanay.tsabary@gmail.com>
2024-03-27 20:17:40 +0200

    Edited layout

--- new-app-files/src/main.tsx
+++ new-app-files/src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { generateBackgroundPattern } from "mynders";
 
 import Plugin from "./Plugin.tsx";
 import MyndersContainer from "./layout/MyndersContainer.tsx";
@@ -11,26 +10,19 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <div
-      style={{
-        height: "100vh",
-        ...generateBackgroundPattern("#e6ecf7", "#459af7"),
-      }}
-    >
-      <MyndersContainer>
-        <Plugin
-          user={{ _id: "Aa1234", email: "user@example.com" }}
-          folderId="dummy-folder-id"
-          encryptData={(data) => "mock encrypted data: " + data}
-          encryptFile={() =>
-            Promise.resolve(new Blob([], { type: "application/octet-stream" }))
-          }
-          decryptData={(data) => "Mock decrypted data: " + data}
-          decryptFile={() =>
-            Promise.resolve(new File([""], "dummy.txt", { type: "text/plain" }))
-          }
-        />
-      </MyndersContainer>
-    </div>
+    <MyndersContainer>
+      <Plugin
+        user={{ _id: "Aa1234", email: "user@example.com" }}
+        folderId="dummy-folder-id"
+        encryptData={(data) => "mock encrypted data: " + data}
+        encryptFile={() =>
+          Promise.resolve(new Blob([], { type: "application/octet-stream" }))
+        }
+        decryptData={(data) => "Mock decrypted data: " + data}
+        decryptFile={() =>
+          Promise.resolve(new File([""], "dummy.txt", { type: "text/plain" }))
+        }
+      />
+    </MyndersContainer>
   </React.StrictMode>
 );
